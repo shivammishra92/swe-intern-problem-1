70bf44341d7ff9a6a0895ff876c50ecfb99cdfd9
Shivam Mishra <shivam552mishra@gmail.com>
2024-11-13 20:57:35 +0530

    Revert "completed the project containerization left"
    This reverts commit b6eacfca79c9c08e4c9db568fe53256ea2bb1686.

--- routes/command.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {storeCommand,searchCommand} from '../controllers/command.controller.js'
-
-const router = express.Router();
-
-router.post('/store',storeCommand);
-router.get('/search',searchCommand);
-
-export default router;
